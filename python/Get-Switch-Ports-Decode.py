e2ac5085db5339916df4de976efef4d281efa4bf
Andrew Longyear <alongyea@cisco.com>
2020-05-14 19:29:42 +0100

    Added Error checking

--- python/Get-Switch-Ports-Decode.py
+++ python/Get-Switch-Ports-Decode.py
@@ -11,12 +11,14 @@ headers = {
 
 response = requests.request("GET", url, headers=headers, data = payload)
 
-#print(response.text.encode('utf8'))
+if (response.status_code) ==200:
+    inv_data=json.loads(response.text)
+    print("{0:<20} {1:>30} {2:>20}". format("Port Number", "Port Name", "VLAN"))
 
-inv_data=json.loads(response.text)
-print("{0:<20} {1:>30} {2:>20}". format("Port Number", "Port Name", "VLAN"))
+    for i in inv_data:
+        if i['name'] == None:
+            i['name'] = 'Blank'
+        print("{0:<20} {1:>30} {2:>20}". format(i['number'], i['name'], i['vlan']))
 
-for i in inv_data:
-    if i['name'] == None:
-        i['name'] = 'Blank'
-    print("{0:<20} {1:>30} {2:>20}". format(i['number'], i['name'], i['vlan']))
+else:
+    print("Unexpected status code: " + str(response.status_code))
